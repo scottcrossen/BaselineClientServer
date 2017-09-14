f6b0f46870191f2baccad3d5783d80b81d4598a6
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-09-14 13:09:12 -0600

    small docker changes

--- docker/jvm/docker-entrypoint.sh
+++ docker/jvm/docker-entrypoint.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-set -e
 
 function finish {
-	kill $PID
-	kill $pid
+	if [ $PID1 -ge 0 ]; then
+		kill $PID1
+	fi
+	kill $PID2
 }
 
 trap 'finish' SIGTERM
 
+PID1=-1
 while true; do
-	if [ -f ${JAR_NAME} ]; then
-		java -jar ${JAR_NAME} &
-	  PID=$!
+	if [ ! -f ${JAR_NAME} ]; then
+		echo -e "\n[INFO] No Jar file found. Waiting for jar before restarting JVM\n"
 	fi
+	while [ ! -f ${JAR_NAME} ]; do sleep 2; done
+	if [ $PID1 -ge 0 ]; then
+		echo -e "\n[INFO] Restarting JVM\n"
+	  kill $PID1
+		PID1=-1
+	fi
+	java -jar ${JAR_NAME} &
+  PID1=$!
   inotifywait -e modify -e delete -e create -e attrib ${JAR_NAME}
-	echo "\nRestarting JVM in 10 seconds\n"
-	sleep 10
-	if [ -f ${JAR_NAME} ]; then
-	  kill $PID
-	fi
 done
-pid=$!
+PID2=$!
 
 wait
